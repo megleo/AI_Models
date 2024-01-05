f8593d230612d4770ab3ce94370ffd794225938c
menglei0427 <menglei0427@thundersoft.com>
2024-01-05 15:15:38 +0800

    Yolov5s Mask train

---
+++ yolov5-6.0/testcode.py
@@ -0,0 +1,33 @@
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+"""
+Train a YOLOv5 model on a custom dataset
+
+Usage:
+    $ python path/to/train.py --data coco128.yaml --weights yolov5s.pt --img 640
+"""
+
+import argparse
+import logging
+import math
+import os
+import random
+import sys
+import time
+from copy import deepcopy
+from pathlib import Path
+
+import numpy as np
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import yaml
+from torch.cuda import amp
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.optim import Adam, SGD, lr_scheduler
+from tqdm import tqdm
+
+FILE = Path(__file__).resolve()
+print(FILE)
+
+ROOT = FILE.parents[0]
+print(ROOT)
